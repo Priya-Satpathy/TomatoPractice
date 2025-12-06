f9522cd159eca5f9d900f209bd056b8a2d20284e
Priya-Satpathy <priyasatpathy429@gmail.com>
2025-12-06 22:35:49 +0530

    content added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,61 +1,59 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
-
-const Navbar = ({setShowLogin}) => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
 
   return (
-    <div className='navbar'>
-    <Link to = '/'><img src={assets.logo} alt="" className='logo' /></Link>
+    <div className="navbar">
+      <Link to="/"><img src={assets.logo} alt="" className="logo" /></Link>
 
-      <ul className='navbar-menu'>
-        <li 
-          onClick={() => setMenu("home")} 
-          className={menu === "home" ? "active" : ""}
-        >
+      <ul className="navbar-menu">
+        <Link to="/" onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>
           home
-        </li>
+        </Link>
 
-        <li 
-          onClick={() => setMenu("menu")} 
+        <a
+          href='#explore-menu'
+          onClick={() => setMenu("menu")}
           className={menu === "menu" ? "active" : ""}
         >
           menu
-        </li>
+        </a>
 
-        <li 
-          onClick={() => setMenu("mobile-app")} 
+        <a
+          href='#app-download'
+          onClick={() => setMenu("mobile-app")}
           className={menu === "mobile-app" ? "active" : ""}
         >
           mobile-app
-        </li>
+        </a>
 
-        <li 
-          onClick={() => setMenu("contact-us")} 
+        <a
+          href='#footer'
+          onClick={() => setMenu("contact-us")}
           className={menu === "contact-us" ? "active" : ""}
         >
           contact us
-        </li>
+        </a>
       </ul>
 
-      <div className='navbar-right'>
-        <Link to='/cart'>
+      <div className="navbar-right">
+        <Link to="cart">
           <img src={assets.search_icon} alt="" />
         </Link>
 
-
-        <div className='navbar-serach-icon'>
-          <img src={assets.basket_icon} alt="" />
-          <div className='dot'></div>
+        <div className="navbar-serach-icon">
+          <Link to="/cart"><img src={assets.basket_icon} alt="" /></Link>
+          <div className="dot"></div>
         </div>
 
-        <button onClick={()=>setShowLogin(true)}>sign in</button>
+        <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
