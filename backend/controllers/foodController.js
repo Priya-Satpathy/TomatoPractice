06f0e5c1a48734bbceaa7cabf420bb2196e3be6d
Priya Satpathy <priyasatpathy429@gmail.com>
2026-05-26 00:01:29 +0530

    content added

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -5,6 +5,23 @@ import fs from 'fs';
 
 //add food items
 const addFood = async (req,res)=>{
+    let image_filename = `${req.file.filename}`;
 
+    const food = new foodModel({
+        name:req.body.name,
+        description:req.body.description,
+        price:req.body,
+        category:req.body.category,
+        image:image_filename
+
+    })
+    try{
+        await food.save();
+        res.json({success:true, message:"Food Added"})
+    }catch(error){
+        console.log(error);
+        res,json({success:false,message:"Error"})
+        
+    }
 }
 export {addFood}
